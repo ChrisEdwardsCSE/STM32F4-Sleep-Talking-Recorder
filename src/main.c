35cdec3a57277b7302693dcf953dc6dbc6dcbeb9
Christopher Edwards <66543275+chedwards492@users.noreply.github.com>
2024-08-26 11:21:04 -0700

    Trying to use selective noise detection

--- src/main.c
+++ src/main.c
@@ -1,1 +1,628 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "fatfs.h"
 
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+#include "spi-driver.h"
+#include "sdcard_audio.h"
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+#define ADC_BUF_LEN 4096
+#define THRESHOLD 2400
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+ADC_HandleTypeDef hadc1;
+DMA_HandleTypeDef hdma_adc1;
+
+TIM_HandleTypeDef htim3;
+TIM_HandleTypeDef htim4;
+
+UART_HandleTypeDef huart2;
+
+/* USER CODE BEGIN PV */
+spi_handler_t spi1_handler;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_DMA_Init(void);
+static void MX_USART2_UART_Init(void);
+static void MX_ADC1_Init(void);
+static void MX_TIM3_Init(void);
+static void MX_TIM4_Init(void);
+/* USER CODE BEGIN PFP */
+
+uint16_t adc_buf[ADC_BUF_LEN]; // note this is 4096 words, so 8192 bytes
+
+extern FATFS fatfs; // fatfs handler;
+extern FIL fil; // file handler
+extern FRESULT f_result;
+extern uint32_t fil_size;
+
+uint8_t button_flag,				// Button pushed
+		start_listening_flag,		// Start/Stop' listening
+		noise_flag,					// Noise detected
+		dma_half_full,				// DMA Half Full flag
+		dma_full;					// DMA Full flag
+uint16_t adc_buf_index;
+uint8_t sdcard_write_en;
+
+/* Enumerated type for device's state */
+enum device_state_enum
+{
+	OFF,
+	LISTENING,
+	RECORDING
+} device_state;
+
+uint8_t print_recording = 0;
+uint8_t print_stopped_recording = 0;
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+// DMA half full
+void HAL_ADC_ConvHalfCpltCallback(ADC_HandleTypeDef *hadc)
+{
+	dma_half_full = 1;
+}
+
+// DMA full
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef *hadc)
+{
+	dma_full = 1;
+}
+
+// Executes an ADC reading at 44.1kHz
+void TIM3_IRQHandler(void)
+{
+	__HAL_TIM_CLEAR_FLAG(&htim3, TIM_FLAG_CC1);
+//	HAL_ADC_Start_DMA(&hadc1, (uint32_t *)&adc_buf, ADC_BUF_LEN); // Perform 1 reading
+
+	// Reset index if DMA at end of buffer
+	if (adc_buf_index == 4096)
+	{
+		adc_buf_index = 0;
+	}
+
+	// If noise detected, enable SD Card write
+	if (adc_buf[adc_buf_index] >= THRESHOLD) // adc_buf_index init'd to 0 before anything so stays in track with DMA buf's index
+	{
+		// Reset timer if hear noise while recording
+		if (device_state == RECORDING)
+		{
+			TIM4->CNT = 0;
+		}
+		// Start timer if hear noise while listening
+		else if (device_state == LISTENING)
+		{
+			device_state = RECORDING;
+			HAL_TIM_Base_Start_IT(&htim4); // Start no noise timer
+
+			print_recording = 1;
+		}
+	}
+	adc_buf_index++;
+}
+
+// Count 5 seconds
+void TIM4_IRQHandler(void)
+{
+	__HAL_TIM_CLEAR_FLAG(&htim4, TIM_FLAG_CC1);
+	// Turn off writing to SD Card
+	device_state = LISTENING;
+	HAL_TIM_Base_Stop_IT(&htim4); // Stop the timer if it expired
+
+	print_stopped_recording = 1;
+}
+
+// B1 Button switches devices between states OFF and LISTENING
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+	if (GPIO_Pin == (1 << 13))
+	{
+		button_flag = 1;
+		HAL_GPIO_TogglePin(GPIOA, 5);
+	}
+}
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_USART2_UART_Init();
+  MX_FATFS_Init();
+  MX_ADC1_Init();
+  MX_TIM3_Init();
+  MX_TIM4_Init();
+  /* USER CODE BEGIN 2 */
+
+
+  DBGMCU->APB1FZ |= DBGMCU_APB1_FZ_DBG_TIM3_STOP;
+  DBGMCU->APB1FZ |= DBGMCU_APB1_FZ_DBG_TIM4_STOP;
+
+  // Initialize SPI1
+  spi_init_t spi1_init_handler;
+  spi1_init_handler.CLKPhase = 0;
+  spi1_init_handler.CLKPolarity = 0;
+  spi1_init_handler.Prescaler = 0b111;
+  spi1_init_handler.DataSize = 0;
+  spi1_init_handler.FirstBit = 0;
+  spi1_init_handler.Mode = 1;
+  spi1_init_handler.NSS = 1;
+  spi1_init_handler.Direction = 0;
+
+  spi1_handler.Instance = SPI1;
+  spi1_handler.Init = spi1_init_handler;
+  SPI_Init(&spi1_handler);
+
+  MX_FATFS_Init();
+
+  myprintf("test print\r\n");
+  HAL_Delay(1000);
+
+  adc_buf_index = 0;
+  device_state = OFF;
+
+
+
+	/** Registers the work area; the filesystem object. Needed before any
+	* file/folder operations. "" path means the default drive
+	*/
+	sdcard_init();
+	HAL_Delay(1000);
+
+	FILINFO finfo;
+	sdcard_check_001wav(&finfo);
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+	uint16_t i = 0;
+
+  while (1)
+  {
+
+	  // Button pushed
+	  if (button_flag)
+	  {
+		  // Stop listening
+		  if (device_state != OFF)
+		  {
+			  device_state = OFF;
+			  HAL_ADC_Stop_DMA(&hadc1);
+			  HAL_TIM_Base_Stop_IT(&htim3);
+			  stop_recording();
+//			  myprintf("Stopped Listening\r\n");
+
+		  }
+		  // Start listening
+		  else
+		  {
+			  device_state = LISTENING;
+			  myprintf("Started Listening\r\n");
+			  start_recording(44099);
+			  HAL_ADC_Start_DMA(&hadc1, (uint32_t *)&adc_buf, ADC_BUF_LEN); // adc_buf is words, but gets passed as dword (32 bits)
+			  HAL_TIM_Base_Start_IT(&htim3); // Start timer for 44.1kHz ADC sampling rate
+		  }
+		  button_flag = 0; // Reset button flag
+	  }
+
+	  /**
+	   * If in listening state, and there was noise detected (sdcard_write_en), and DMA half/completely full,
+	   * write out the contents to the SD card
+	   */
+	  if (device_state == RECORDING && dma_half_full)
+	  {
+		  sdcard_wav_write((uint8_t *)adc_buf, ADC_BUF_LEN);
+		  dma_half_full = 0;
+	  }
+	  if (device_state == RECORDING && dma_full)
+	  {
+		  sdcard_wav_write((uint8_t *)adc_buf + ADC_BUF_LEN, ADC_BUF_LEN);
+		  dma_full = 0;
+	  }
+
+	  // how to check if should write the stuff to file
+
+
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 84;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 2;
+  RCC_OscInitStruct.PLL.PLLR = 2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief ADC1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_ADC1_Init(void)
+{
+
+  /* USER CODE BEGIN ADC1_Init 0 */
+
+  /* USER CODE END ADC1_Init 0 */
+
+  ADC_ChannelConfTypeDef sConfig = {0};
+
+  /* USER CODE BEGIN ADC1_Init 1 */
+
+  /* USER CODE END ADC1_Init 1 */
+
+  /** Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
+  */
+  hadc1.Instance = ADC1;
+  hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV4;
+  hadc1.Init.Resolution = ADC_RESOLUTION_12B;
+  hadc1.Init.ScanConvMode = DISABLE;
+  hadc1.Init.ContinuousConvMode = DISABLE;
+  hadc1.Init.DiscontinuousConvMode = DISABLE;
+  hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_RISING;
+  hadc1.Init.ExternalTrigConv = ADC_EXTERNALTRIGCONV_T3_CC1;
+  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc1.Init.NbrOfConversion = 1;
+  hadc1.Init.DMAContinuousRequests = ENABLE;
+  hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+  if (HAL_ADC_Init(&hadc1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
+  */
+  sConfig.Channel = ADC_CHANNEL_0;
+  sConfig.Rank = 1;
+  sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN ADC1_Init 2 */
+
+  /* USER CODE END ADC1_Init 2 */
+
+}
+
+/**
+  * @brief TIM3 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM3_Init(void)
+{
+
+  /* USER CODE BEGIN TIM3_Init 0 */
+
+  /* USER CODE END TIM3_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
+
+  /* USER CODE BEGIN TIM3_Init 1 */
+
+  /* USER CODE END TIM3_Init 1 */
+  htim3.Instance = TIM3;
+  htim3.Init.Prescaler = 3;
+  htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim3.Init.Period = 635;
+  htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim3, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_OC_Init(&htim3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_TIMING;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  if (HAL_TIM_OC_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM3_Init 2 */
+
+  /* USER CODE END TIM3_Init 2 */
+
+}
+
+/**
+  * @brief TIM4 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM4_Init(void)
+{
+
+  /* USER CODE BEGIN TIM4_Init 0 */
+
+  /* USER CODE END TIM4_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
+
+  /* USER CODE BEGIN TIM4_Init 1 */
+
+  /* USER CODE END TIM4_Init 1 */
+  htim4.Instance = TIM4;
+  htim4.Init.Prescaler = 65535-1;
+  htim4.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim4.Init.Period = 6409;
+  htim4.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim4.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim4, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_OC_Init(&htim4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim4, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_TIMING;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  if (HAL_TIM_OC_ConfigChannel(&htim4, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM4_Init 2 */
+
+  /* USER CODE END TIM4_Init 2 */
+
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+  * Enable DMA controller clock
+  */
+static void MX_DMA_Init(void)
+{
+
+  /* DMA controller clock enable */
+  __HAL_RCC_DMA2_CLK_ENABLE();
+
+  /* DMA interrupt init */
+  /* DMA2_Stream0_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA2_Stream0_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA2_Stream0_IRQn);
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin : B1_Pin */
+  GPIO_InitStruct.Pin = B1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
+
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
---
+++ src/sdcard_audio.c
@@ -0,0 +1,234 @@
+/*
+ * sdcard_audio.c
+ *
+ *  Created on: Aug 20, 2024
+ *      Author: ched
+ */
+#include "sdcard_audio.h"
+
+/**
+ * 0-3 RIFF {0x52, 0x49, 0x46, 0x46}
+ * 4-7 size of file (bytes) {data_sec size + 36}
+ * 8-11 file type header "wave" {0x57, 0x41, 0x56, 0x45}
+ * 12-15 "fmt" {0x66, 0x6d, 0x74, 0x20}
+ * 16-19 length of format data {0x10, 0x00, 0x00, 0x00} (16)
+ * 20-21 type of fomart "PCM" (1) {0x01, 0x00}
+ * 22-23 num channels (2) {0x02, 0x00}
+ * 24-27 sample rate (32kHz) {0x80, 0x7d, 0x00, 0x00}
+ * 28-31 byte rate; sample rate * Bpsample * channels (19200 it's actually 128000 tho) {0x00, 0xf4, 0x01, 0x00}
+ * 32-33 Bpsample * channels (4) {0x04, 0x00}
+ * 34-35 bits per sample (16) {0x10, 0x00}
+ * 36-39 data {0x64, 0x61, 0x74, 0x61} data chunk header, marks beginning of data section
+ * 40-43 size of data section {data section size}
+ * THEN comes the data
+ */
+static uint8_t fil_header [44]={0x52, 0x49, 0x46, 0x46,
+		0xa4, 0xa9, 0x03, 0x00,
+		0x57, 0x41, 0x56, 0x45,
+		0x66, 0x6d, 0x74, 0x20,
+		0x10, 0x00, 0x00, 0x00,
+		0x01, 0x00,
+		0x01, 0x00, // this WAS 0x02, 0x00
+		0x43, 0xac, 0x00, 0x00, // (44099)
+		0x00, 0xf4, 0x01, 0x00,
+		0x02, 0x00, // this WAS 0x04 0x00 bc 2 * 2
+		0x0c, 0x00, // this WAS 0x10 0x00 (16)
+		0x64, 0x61, 0x74, 0x61,
+		0x80, 0xa9, 0x03, 0x00};
+
+static FATFS fatfs; // fatfs handler;
+static FIL fil; // file handler
+static FRESULT f_result;
+static uint32_t fil_size;
+static uint8_t first_time = 0;
+
+extern UART_HandleTypeDef huart2;
+
+void myprintf(const char *fmt, ...)
+{
+	static char buffer[256];
+	va_list args;
+	va_start(args, fmt);
+	vsnprintf(buffer, sizeof(buffer), fmt, args);
+	va_end(args);
+
+	int len = strlen(buffer);
+	HAL_UART_Transmit(&huart2, (uint8_t*)buffer, len, 100);
+}
+
+/**
+ * Start recording of audio data. Creates the file and updates the frequency-related
+ * data wtihin the file header.
+ *
+ * @param freq - the frequency of recording
+ */
+void start_recording(uint32_t freq)
+{
+	// Initialize file name
+	static char file_name[] = "w_000.wav";
+	static uint8_t file_count = 1;
+	int file_num_digits = file_count;
+
+	uint32_t byte_rate = freq * 2 * 1; // byte rate = Bpsample * channels
+	fil_header[24] = (uint8_t) (freq);
+	fil_header[25] = (uint8_t) (freq >> 8);
+	fil_header[26] = (uint8_t) (freq >> 16);
+	fil_header[27] = (uint8_t) (freq >> 24);
+	fil_header[28] = (uint8_t) (byte_rate);
+	fil_header[29] = (uint8_t) (byte_rate >> 8);
+	fil_header[30] = (uint8_t) (byte_rate >> 16);
+	fil_header[31] = (uint8_t) (byte_rate >> 24);
+
+	// Define wave file name
+	file_name[4] = file_num_digits % 10 + 48; // ASCII 48 = '0'
+	file_num_digits /= 10;
+	file_name[3] = file_num_digits % 10 + 48;
+	file_num_digits /= 10;
+	file_name[2] = file_num_digits % 10 + 48;
+	file_num_digits /= 10;
+	file_count++;
+
+	// Create the file to be written to
+	f_result = f_open(&fil, file_name, FA_WRITE | FA_CREATE_ALWAYS);
+	if (f_result != 0)
+	{
+		myprintf("Error start_recording: %d\n", f_result);
+		return;
+	}
+	else
+	{
+		myprintf("start_recording success\n");
+	}
+	fil_size = 0;
+}
+
+/**
+ * Updates wav file's meta data, ie the new size of the file and the size of the
+ * data. Then closes the file and resets first_time so that
+ */
+void stop_recording(void)
+{
+	uint16_t temp_num;
+	fil_size -= 8; // remove byte 0-7 cuz they're not considered as part of file
+	fil_header[4] = (uint8_t) (fil_size);
+	fil_header[5] = (uint8_t) (fil_size >> 8);
+	fil_header[6] = (uint8_t) (fil_size >> 16);
+	fil_header[7] = (uint8_t) (fil_size >> 24);
+	fil_size -= 36; // remove rest of header bytes from count; only want the data
+	fil_header[40] = (uint8_t) (fil_size);
+	fil_header[41] = (uint8_t) (fil_size >> 8);
+	fil_header[42] = (uint8_t) (fil_size >> 16);
+	fil_header[43] = (uint8_t) (fil_size >> 24);
+
+
+	f_lseek(&fil, 0); // jump to beginning of file to update format to write header
+	f_write(&fil, (void *)fil_header, sizeof(fil_header),(UINT*)&temp_num);
+	if (f_result != 0)
+	{
+		myprintf("Error stop_recording: %d\n", f_result);
+		return;
+	}
+	else
+	{
+		myprintf("stop_recording success\n");
+	}
+	f_close(&fil);
+	first_time = 0;
+	// finished
+}
+
+/**
+ * Writes data to opened wav file. Called when DMA buffer with values reaches
+ * half or full capacity. When called for the firs time after a file is opened,
+ * writes the file header meta data to the file. Updates file_size global variable
+ *
+ * @param data - the data to write to the SD Card
+ * @param data_size - the size of the data to write in bytes
+ */
+void sdcard_wav_write(uint8_t *data, uint16_t data_size)
+{
+	uint16_t temp_num;
+	// Initialize the file header on the first write to wav file
+	if (first_time == 0)
+	{
+		for (int i = 0; i < 44; i++)
+		{
+			*(data + i) = fil_header[i];
+		}
+		first_time = 1;
+	}
+	// Write passed data to data section of file
+	f_result = f_write(&fil, (void *)data, data_size, (UINT *)&temp_num);
+	if (f_result != 0)
+	{
+		myprintf("Error sdcard_wav_write: %d\n", f_result);
+		return;
+	}
+	else
+	{
+		myprintf("sdcard_wav_write success\n");
+	}
+	fil_size += data_size; // update wav file's data size for its header
+}
+
+/**
+ * Just mount the SD Card
+ */
+void sdcard_init()
+{
+	f_result = f_mount(&fatfs, "", 1);
+	if (f_result != 0)
+	{
+		myprintf("Error sdcard_init, %d\n", f_result);
+		return;
+	}
+	else
+	{
+		myprintf("sdcard_init success\n");
+	}
+}
+
+void sdcard_read_stats()
+{
+	DWORD free_clusters, free_sectors, total_sectors;
+
+	FATFS* getFreeFs;
+
+	/**
+	* get num of free clusters on card. set in
+	* path = "" is default drive. puts the value in free_clusters
+	*/
+	f_result = f_getfree("", &free_clusters, &getFreeFs);
+	if (f_result != FR_OK)
+	{
+		myprintf("f_getfree error (&i)\r\n", f_result);
+		while(1);
+	}
+	// total sectors = (num of FAT entries - 2) * num clusters
+	total_sectors = (getFreeFs->n_fatent - 2) * getFreeFs->csize;
+	// num free sectors = num free clusters
+	free_sectors = free_clusters * getFreeFs->csize;
+
+	myprintf("sd card stats:\r\n%10lu KiB total drive space. \r\n%10lu KiB availabile.\r\n", total_sectors/2, free_sectors/2);
+
+	f_close(&fil);
+}
+
+void sdcard_check_001wav(FILINFO *file_info)
+{
+	const char *file_name = "w_001.wav";
+	f_result = f_stat(file_name, &file_info);
+	if (f_result == FR_OK)
+	{
+		myprintf("found w_001.wav, and deleting it");
+		f_result = f_unlink(file_name);
+		if (f_result == FR_OK)
+		{
+			myprintf("delete w_001.wav");
+		}
+	}
+	else if (f_result == FR_NO_FILE)
+	{
+		myprintf("could not find w_001.wav");
+	}
+}
